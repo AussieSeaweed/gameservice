bb3f6c018e86a8159225f400a1b12d0c55d32251
Juho Kim <juho-kim@outlook.com>
2020-09-01 17:49:09 -0400

    Fixed Typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gameservice",
-    version="0.0.9",
+    version="0.0.10",
     author="Juho Kim",
     author_email="juho-kim@outlook.com",
     description="A package for game services on python",
