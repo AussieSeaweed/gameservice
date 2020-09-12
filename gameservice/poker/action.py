b54ba663237f9116e5b6d1b02a219b1ae06d197a
Juho Kim <juho-kim@outlook.com>
2020-09-11 22:52:02 -0400

    Fixed Bug

--- gameservice/poker/action.py
+++ gameservice/poker/action.py
@@ -179,13 +179,13 @@ class Showdown(PokerNatureAction):
                     self.game.chance_players.append(self.game.players[i % len(self.game.players)])
 
         chance_player = self.game.chance_players.pop(0)
-        self.game.results[chance_player.hand].append(chance_player)
 
         for hand, players in self.game.results.items():
             if hand < chance_player.hand and max(player.commitment for player in players) >= chance_player.commitment:
                 chance_player.muck()
                 break
         else:
+            self.game.results[chance_player.hand].append(chance_player)
             chance_player.expose()
 
         if not self.game.chance_players:
