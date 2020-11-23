3101d1bc4d3a8db2bc162e0e39de9655a5e83ac9
Juho <juho-kim@outlook.com>
2020-11-23 06:14:09 -0500

    Updated Code styling

--- gameservice/game/exception.py
+++ gameservice/game/exception.py
@@ -1,33 +1,33 @@
 class GameException(Exception):
-    """Base exception class for exceptions related to gameservice"""
+    """Base exception class for exceptions related to gameservice."""
     pass
 
 
 class GameTerminalException(GameException):
-    """The terminality of the game is invalid"""
+    """The terminality of the game is invalid."""
     pass
 
 
 class GameInterruptionException(GameException):
-    """The game has already been modified"""
+    """The game has already been modified."""
     pass
 
 
 class GamePlayerException(GameException):
-    """The player cannot perform the action"""
+    """The player cannot perform the action."""
     pass
 
 
 class GameParameterException(GameException):
-    """One or more invalid parameters are supplied to the game"""
+    """One or more invalid parameters are supplied to the game."""
     pass
 
 
 class GameActionException(GameException):
-    """The action is invalid"""
+    """The action is invalid."""
     pass
 
 
 class GameActionArgumentException(GameException):
-    """One or more invalid arguments are supplied to the action"""
+    """One or more invalid arguments are supplied to the action."""
     pass
