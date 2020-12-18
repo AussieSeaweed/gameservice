7a8e99d0ff4df4e603d8dcfb584358bdcfa70705
Juho <juho-kim@outlook.com>
2020-12-18 12:06:13 -0500

    Added tic tac toe interaction

---
+++ gameservice/tictactoe/tests/interact.py
@@ -0,0 +1,10 @@
+from gameservice.game.tests.interact import interact_seq
+from gameservice.tictactoe import TTTGame
+
+
+def main():
+    interact_seq(TTTGame)
+
+
+if __name__ == '__main__':
+    main()
