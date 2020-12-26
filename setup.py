0b16ccf3b8ae64d471e145b65ee51389f3d56d23
Juho <juho-kim@outlook.com>
2020-12-26 18:39:51 -0500

    Changed version number to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='gameframe',
-    version='0.0.1',
+    version='0.0.2',
     author='Juho Kim',
     author_email='juho-kim@outlook.com',
     description='A package for various game implementations on python',
