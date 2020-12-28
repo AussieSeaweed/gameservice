4d9e17039065064142b700668677642e78020046
Juho <juho-kim@outlook.com>
2020-12-27 21:37:06 -0500

    Formatted files

--- gameframe/sequential/actions.py
+++ gameframe/sequential/actions.py
@@ -1,19 +1,15 @@
 from abc import ABC
 
 from .games import SequentialGame
-from ..game import Action, E, N, P
 from .utils import G
+from ..game import Action, E, N, P
 
 
 class SequentialAction(Action[G, E, N, P], ABC):
     """SequentialAction is the abstract base class for all sequential actions."""
 
-    def verify(self) -> None:
-        """
-        :return: None
-        :raise TypeError: if the game is not an instance of SequentialGame
-        """
-        super().verify()
+    def _verify(self) -> None:
+        super()._verify()
 
         if not isinstance(self.game, SequentialGame):
             raise TypeError('The game is not an instance of SequentialGame')
