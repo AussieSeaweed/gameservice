cd6265c7f3b86c4544d582479fd335c9d10363ca
Juho <juho-kim@outlook.com>
2020-12-28 09:33:43 -0500

    Finished tic tac toe

--- gameframe/game/tests/interact.py
+++ gameframe/game/tests/interact.py
@@ -1,6 +1,28 @@
-from json import dumps
-from typing import Any, Dict
+from typing import Any
 
 
-def print_information_set(information_set: Dict[str, Any], indent=4):
-    print(dumps(information_set, indent=indent))
+def pretty_print(o: Any, indent: str = '    ', prefix: str = '') -> None:
+    """Prints the object on the console prettily.
+
+    :param o: the object
+    :param indent: the indentation string
+    :param prefix: the prefix string
+    :return: None
+    """
+    if isinstance(o, list):
+        print(prefix + '[')
+
+        for value in o:
+            pretty_print(value, indent, prefix + indent)
+
+        print(prefix + ']')
+    elif isinstance(o, dict):
+        print(prefix + '{')
+
+        for key, value in o.items():
+            print(prefix + indent + str(key) + ':')
+            pretty_print(value, indent, prefix + indent + indent)
+
+        print(prefix + '}')
+    else:
+        print(prefix + str(o))
--- gameframe/sequential/tests/interact.py
+++ gameframe/sequential/tests/interact.py
@@ -1,22 +1,23 @@
-from gameframe.game.tests import print_information_set
+from typing import Callable
+
+from gameframe.game import Action, E, List, N, P
+from gameframe.game.tests import pretty_print
+from gameframe.sequential import G, SequentialGame
 
 
-def sequential_interact(sequential_game_factory):
+def sequential_interact(sequential_game_factory: Callable[[], SequentialGame[G, E, N, P]]) -> None:
     """Interacts with sequential games on console.
 
     :param sequential_game_factory: a function that creates a sequential game instance
     :return: None
     """
-    sequential_game = sequential_game_factory()
+    sequential_game: SequentialGame[G, E, N, P] = sequential_game_factory()
 
     while not sequential_game.terminal:
-        print_information_set(sequential_game.player.info_set)
+        pretty_print(sequential_game.player.information_set)
 
-        actions = sequential_game.player.actions
-
-        for i, action in enumerate(actions):
-            print(f'{i}: {action}')
+        actions: List[Action[G, E, N, P]] = sequential_game.player.actions
 
         actions[0 if len(actions) == 1 else int(input('Action #: '))].act()
 
-    print_information_set(sequential_game.nature.info_set)
+    pretty_print(sequential_game.nature.information_set)
