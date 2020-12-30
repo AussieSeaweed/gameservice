e160e6c9151c1ab9d69669c4cf7d976bec2bfa28
Juho Kim <juho-kim@outlook.com>
2020-12-30 15:26:29 -0500

    Migrating to Python 3.8

--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     install_requires=['treys'],
 )
