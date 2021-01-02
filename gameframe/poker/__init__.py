8c8b43cdd1258da15ca18401dfad1c04e0e1593f
Juho Kim <juho-kim@outlook.com>
2021-01-01 21:14:54 -0500

    Configured poker tests

--- gameframe/poker/__init__.py
+++ gameframe/poker/__init__.py
@@ -1,8 +1,8 @@
-from gameframe.poker.actions import AggressiveAction, PassiveAction, PokerNatureAction, PokerPlayerAction, \
-    RoundAction, SubmissiveAction
-from gameframe.poker.bases import PokerAction, PokerEnvironment, PokerGame, PokerNature, PokerPlayer
-from gameframe.poker.games import HoldEmGame, TexasHoldEmGame
+from gameframe.poker.actions import AggressiveAction, PassiveAction, RoundAction, SubmissiveAction
+from gameframe.poker.bases import PokerAction, PokerEnvironment, PokerGame, PokerNature, PokerNatureAction, \
+    PokerPlayer, PokerPlayerAction
+from gameframe.poker.games import HoldEmGame, NoLimitMixin, NoLimitTexasHoldEmGame, TexasHoldEmGame
 from gameframe.poker.limits import FixedLimit, Limit, NoLimit
-from gameframe.poker.rounds import BettingRound, NoLimitBettingRound, Round
+from gameframe.poker.rounds import BettingRound, Round
 from gameframe.poker.utils import Card, Deck, Evaluator, GreekHoldEmEvaluator, Hand, HoleCard, OmahaHoldEmEvaluator, \
     Rank, SixPlusDeck, StandardDeck, StandardEvaluator, Suit
--- gameframe/poker/tests/test_monte_carlo.py
+++ gameframe/poker/tests/test_monte_carlo.py
@@ -1,20 +1,46 @@
 from unittest import TestCase, main
 
+from gameframe.poker import NoLimitTexasHoldEmGame
 from gameframe.sequential.tests import SequentialMonteCarloTestCaseMixin
-# TODO
 
-class NoLimitTexasHoldEmMonteCarloTestCase(TestCase, SequentialMonteCarloTestCaseMixin[TicTacToeGame]):
-    """TicTacToeTestCase is the class for tic tac toe test cases."""
+
+class CustomNoLimitTexasHoldEmGame(NoLimitTexasHoldEmGame):
+    """CustomNoLimitTexasHoldEmGame is the class for custom no-limit texas hold'em games."""
+
+    @property
+    def small_blind(self) -> int:
+        return 1
+
+    @property
+    def big_blind(self) -> int:
+        return 2
+
+    @property
+    def ante(self) -> int:
+        return 0
+
+    @property
+    def starting_stacks(self) -> list[int]:
+        return [100, 200, 300, 200, 300, 100]
+
+    @property
+    def _lazy(self) -> bool:
+        return True
+
+
+class NoLimitTexasHoldEmMonteCarloTestCase(TestCase, SequentialMonteCarloTestCaseMixin[CustomNoLimitTexasHoldEmGame]):
+    """NoLimitTexasHoldEmMonteCarloTestCase is the class for custom no-limit texas hold'em test cases."""
 
     @property
     def _monte_carlo_test_count(self) -> int:
         return 1000
 
-    def _create_game(self) -> TicTacToeGame:
-        return TicTacToeGame()
+    def _create_game(self) -> CustomNoLimitTexasHoldEmGame:
+        return CustomNoLimitTexasHoldEmGame()
 
-    def _verify_game(self, game: TicTacToeGame) -> None:
-        assert game.environment._winner is not None or not game.environment._empty_coordinates
+    def _verify_game(self, game: CustomNoLimitTexasHoldEmGame) -> None:
+        assert sum(game.starting_stacks) == sum(player.stack for player in game.players) \
+               and all(player.stack >= 0 and player.bet >= 0 for player in game.players)
 
 
 if __name__ == '__main__':
