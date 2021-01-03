acccbd7784dd6ef0a80cf8faff262c24dfe5fdea
Juho Kim <juho-kim@outlook.com>
2021-01-03 01:22:27 -0500

    Updated version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='gameframe',
-    version='0.0.2',
+    version='0.0.3',
     author='Juho Kim',
     author_email='juho-kim@outlook.com',
     description='A package for various game implementations on python',
