fcab97f8e4c0dfcbbe22e461d82091b934b9748a
Juho Kim <juho-kim@outlook.com>
2021-01-03 01:09:43 -0500

    Made the code compatible to Python 3.8

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from setuptools import find_packages, setup
 setup(
     name='gameframe',
     version='0.0.2',
-    packages=find_packages(),
-    license=open('LICENSE', 'r').read(),
     author='Juho Kim',
     author_email='juho-kim@outlook.com',
     description='A package for various game implementations on python',
     long_description=open('README.rst', 'r').read(),
-    long_description_content_type='text/markdown',
+    long_description_content_type='text/x-rst',
     url='https://github.com/AussieSeaweed/gameframe',
+    packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
