0a2d374842b6f9575611c7d17724e48971fd42a9
Juho Kim <juho-kim@outlook.com>
2021-01-14 04:12:47 -0500

    Updated documentations

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Juho Kim'
 author = 'Juho Kim'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1dev0'
+release = '0.0.1.dev0'
 
 
 # -- General configuration ---------------------------------------------------
