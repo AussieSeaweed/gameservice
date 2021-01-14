2e2978a1ed211a7f30baf4778eaddd86b849d508
Juho Kim <juho-kim@outlook.com>
2021-01-14 03:48:37 -0500

    Set version to 0.0.1dev0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Juho Kim'
 author = 'Juho Kim'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.5'
+release = '0.0.1dev0'
 
 
 # -- General configuration ---------------------------------------------------
