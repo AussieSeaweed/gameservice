b153365eda4a494f92463c47a35b31bacd9bb154
Juho Kim <juho-kim@outlook.com>
2021-01-14 17:35:58 -0500

    Updated version number to 0.0.1dev1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Juho Kim'
 author = 'Juho Kim'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1.dev0'
+release = '0.0.1.dev1'
 
 
 # -- General configuration ---------------------------------------------------
