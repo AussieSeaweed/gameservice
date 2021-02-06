bb316b66746eaa711676a8050378b664fd29131e
Juho Kim <juho-kim@outlook.com>
2021-02-06 07:50:28 -0500

    Fixed typo

--- docs/source/examples/create.py
+++ docs/source/examples/create.py
@@ -5,14 +5,14 @@ ante = 1
 blinds = [1, 2]
 starting_stacks = [200, 200, 300]
 
-# Create a no-limit texas hold'em game
+# Create a no-limit Texas Hold'em game
 nlthe_game = NLTHEGame(ante, blinds, starting_stacks)
 
-# Create a no-limit greek hold'em game
-nlghe_game = NLOHEGame(ante, blinds, starting_stacks)
+# Create a no-limit Omaha Hold'em game
+nlohe_game = NLOHEGame(ante, blinds, starting_stacks)
 
-# Create a no-limit omaha hold'em game
-nlohe_game = NLGHEGame(ante, blinds, starting_stacks)
+# Create a no-limit Greek Hold'em game
+nlghe_game = NLGHEGame(ante, blinds, starting_stacks)
 
 # Create a tic tac toe game
 ttt_game = TTTGame()
