7ac7abee5430f553da3935815f6bae4b7a6a7920
Juho Kim <juho-kim@outlook.com>
2021-02-08 15:17:48 -0500

    Added py.typed file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Juho Kim'
 author = 'Juho Kim'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.1b1'
 
 # -- General configuration ---------------------------------------------------
 
