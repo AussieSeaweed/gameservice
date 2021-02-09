40b248739bc00abb7ac7e0f44039ee8ffd83c044
Juho Kim <juho-kim@outlook.com>
2021-02-08 23:50:10 -0500

    cached some properties

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Juho Kim'
 author = 'Juho Kim'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1b1'
+release = '0.0.1b2'
 
 # -- General configuration ---------------------------------------------------
 
