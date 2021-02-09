35e188f4236f38be73e10eb838b6429855fb3416
Juho Kim <juho-kim@outlook.com>
2021-02-09 03:02:58 -0500

    Added hole card type

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Juho Kim'
 author = 'Juho Kim'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1b3'
+release = '0.0.1b4'
 
 # -- General configuration ---------------------------------------------------
 
