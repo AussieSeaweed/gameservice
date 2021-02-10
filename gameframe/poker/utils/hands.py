dd7759bd0f016ccf6d64201642668e2017d18024
Juho Kim <juho-kim@outlook.com>
2021-02-10 06:55:38 -0500

    using type ignore comment instead of casting

--- gameframe/poker/utils/hands.py
+++ gameframe/poker/utils/hands.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from collections import Iterable
-from typing import Any, cast
+from typing import Any
 
 from treys import Card as TreysCard, Evaluator as TreysEvaluator  # type: ignore
 
@@ -31,20 +31,20 @@ class TreysHand(Hand):
     treys_evaluator = TreysEvaluator()
 
     def __init__(self, hole_cards: Iterable[Card], board_cards: Iterable[Card]):
-        self.__hand_rank: int = self.treys_evaluator.evaluate(
+        self.__hand_rank = self.treys_evaluator.evaluate(
             list(map(TreysCard.new, map(lambda card: f'{card.rank.value}{card.suit.value}', hole_cards))),
             list(map(TreysCard.new, map(lambda card: f'{card.rank.value}{card.suit.value}', board_cards))),
         )
 
     def __lt__(self, other: Any) -> bool:
         if isinstance(other, TreysHand):
-            return self.__hand_rank > other.__hand_rank
+            return self.__hand_rank > other.__hand_rank  # type: ignore
         else:
             return NotImplemented
 
     def __eq__(self, other: Any) -> bool:
         if isinstance(other, TreysHand):
-            return self.__hand_rank == other.__hand_rank
+            return self.__hand_rank == other.__hand_rank  # type: ignore
         else:
             return False
 
@@ -52,4 +52,5 @@ class TreysHand(Hand):
         return hash(self.__hand_rank)
 
     def __repr__(self) -> str:
-        return cast(str, self.treys_evaluator.class_to_string(self.treys_evaluator.get_rank_class(self.__hand_rank)))
+        return self.treys_evaluator.class_to_string(  # type: ignore
+            self.treys_evaluator.get_rank_class(self.__hand_rank))
