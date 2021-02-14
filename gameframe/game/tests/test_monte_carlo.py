44d3a58a4aaf702582665dae749b07fa28e79edd
Juho <juho-kim@outlook.com>
2021-02-14 15:13:27 -0500

    added some assertions

--- gameframe/game/tests/test_monte_carlo.py
+++ gameframe/game/tests/test_monte_carlo.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Generic
 
+from gameframe.game import BaseActor, BaseGame
 from gameframe.game.generics import G
 
 
@@ -17,7 +18,9 @@ class MCTestCaseMixin(Generic[G], ABC):
                 self.verify(game)
 
     def verify(self, game: G) -> None:
-        pass
+        assert isinstance(game, BaseGame)
+        assert isinstance(game.nature, BaseActor)
+        assert all(isinstance(player, BaseActor) for player in game.players)
 
     @abstractmethod
     def act(self, game: G) -> None:
