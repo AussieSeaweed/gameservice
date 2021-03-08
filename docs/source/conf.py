de33b70b1e4f97b73b7adebd08d262447c8b2b42
Juho <juho-kim@outlook.com>
2021-03-07 19:28:19 -0500

    using new auxiliary

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Juho Kim'
 author = 'Juho Kim'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3a2'
+release = '0.0.3b1'
 
 # -- General configuration ---------------------------------------------------
 
