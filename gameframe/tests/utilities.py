8c6939a5111a5436a90dfb2e6d70dd6f8c927d2a
Juho <juho-kim@outlook.com>
2021-06-29 14:11:21 -0400

    Simplified code

--- gameframe/tests/utilities.py
+++ gameframe/tests/utilities.py
@@ -3,12 +3,18 @@ from time import time
 
 
 class GameFrameTestCaseMixin(ABC):
-    TEST_TIME = 1
+    @property
+    def test_time(self):
+        return 1
+
+    @property
+    def game_name(self):
+        return type(self.create_game()).__name__
 
     def test_monte_carlo(self):
         init_time = time()
 
-        while time() - init_time < self.TEST_TIME:
+        while time() - init_time < self.test_time:
             game = self.create_game()
 
             self.verify(game)
@@ -21,14 +27,14 @@ class GameFrameTestCaseMixin(ABC):
         init_time = time()
         count = 0
 
-        while time() - init_time < self.TEST_TIME:
+        while time() - init_time < self.test_time:
             count += 1
             game = self.create_game()
 
             while not game.is_terminal():
                 self.act(game)
 
-        print(f'{count} {type(self.create_game()).__name__}(s) played in {self.TEST_TIME} second(s).')
+        print(f'{count} {self.game_name}(s) played in {self.test_time} second(s).')
 
     @abstractmethod
     def create_game(self):
